d032495d0897db0f21a72c93e86ee751a00b2871
RMA-MUN <3032747608@qq.com>
2026-03-24 17:14:25 +0800

    fix:修复上传头像错误

--- DjangoUserService/apps/file/views.py
+++ DjangoUserService/apps/file/views.py
@@ -3,14 +3,28 @@ from rest_framework.views import APIView
 from shortuuid import uuid
 from apps.file.serializers import ImgSerializer
 from django.conf import settings
+from apps.user.authentications import JWTAuthentication
+from apps.user.models import User
+from apps.utils.cache_utils import clear_user_cache
 
 import os
 
 class UploadAPIView(APIView):
     """文件上传"""
+    authentication_classes = [JWTAuthentication]
 
     def post(self, request, *args, **kwargs) -> Response:
         """文件上传"""
+        # 验证用户是否已登录
+        if not request.user or not request.user.is_authenticated:
+            return Response(
+                {
+                    "errno": 1,
+                    "message": "请先登录"
+                },
+                status=401
+            )
+        
         serializer = ImgSerializer(data=request.data)
         if serializer.is_valid():
             img = serializer.validated_data['img']
@@ -33,10 +47,23 @@ class UploadAPIView(APIView):
             # 返回文件的URL
             # 示例：http://localhost:8000/media/img/202308251423523423423.png
             file_url = settings.MEDIA_URL + 'img/' + filename
+            
+            # 更新用户的avatar字段
+            try:
+                user = request.user
+                user.avatar = file_url
+                user.save()
+                # 清除用户缓存
+                clear_user_cache(user.uuid)
+            except Exception as e:
+                print(e)
+                # 即使更新用户信息失败，也要返回文件上传成功的响应
+                pass
+            
             # 返回文件的URL
             return Response(
                 {
-                    "errno": 0,
+                    "success": True,
                     "data": {
                         "url": file_url,
                         "alt": "当前加载较为缓慢，请稍后重试",
@@ -47,4 +74,3 @@ class UploadAPIView(APIView):
         else:
             from rest_framework import status
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
